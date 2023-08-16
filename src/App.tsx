1c26a727713b81ac447e93d21216a22ca4824873
Gabriel Neoob <gabrielngc@gmail.com>
2023-08-16 20:01:02 -0300

    :ok_hand: style: remove comment lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,11 @@
 import {  Container,  GlobalStyle } from "./GlobalStyle"
 import Header from './components/header'
-import { useEffect, useState} from "react"
 import { countryContext } from "./GlobalContext"
 import MainRoutes from "./routes/MainRoutes"
 
 
 const App = () => {
   const { state } = countryContext();
-  // const searchCountry = () => {
-  //   const newCountries = countries?.filter((country) => country.name.common.toLowerCase().startsWith(countryInput.toLowerCase()))
-  //   setCountries(newCountries)
-  // }
-
-  // useEffect(() => {
-  //   filterCountry(countries, state.countryInput);
-  // }, [state.countryInput])
-
-  // useEffect(() => {
-  //   loadCountries()
-  // }, [])
-
-
-
   return (
     <>
       <GlobalStyle toggleBg ={state.toggleBg}/>
