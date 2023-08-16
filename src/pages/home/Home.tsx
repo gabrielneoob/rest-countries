cdff68da976e5d4626f02f7fba846145596cc406
Gabriel Neoob <gabrielngc@gmail.com>
2023-08-16 19:56:15 -0300

    :fire: feat: home page

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from 'react'
+import CountryContainer from '../../components/countryContainer'
+import { countryContext } from '../../GlobalContext'
+import CountryCard from '../../components/countryCard'
+import Flex from '../../components/flex'
+import { stylesFlex } from '../../components/flex/styles'
+import Input from '../../components/input'
+import Select from '../../components/select'
+import { api } from '../../api'
+
+
+
+const Home = () => {
+  const { state, changeInput, loadCountriesByRegion, countriesByRegion } = countryContext();
+  const [testSelect, setTestSelect] = useState('all')
+
+  useEffect(() => {
+    loadCountriesByRegion(testSelect)
+    console.log(countriesByRegion)
+  }, [testSelect])
+
+
+  return (
+    <>
+      <Flex css={stylesFlex.containerInputSelect}>
+          <Input
+          onChange={(e) => changeInput(e)}
+          value={state.countryInput}
+          placeHolder="Search for a country..." 
+          />
+          <Select name="country" onChange={(e) => setTestSelect(e.target.value)}>
+            <option value="all" >Filter by Region</option>
+            {api.countries.map((countrie) => <option 
+            value={countrie}
+            key={countrie.toLowerCase()}
+            >
+            {countrie}
+            </option>)}
+          </Select>
+        </Flex>
+      <CountryContainer>
+        {state.countryInput ? countriesByRegion.filter((country) => country.name.common.toLowerCase().startsWith(state.countryInput.toLowerCase())).map((country) => <CountryCard data={country}/>) : countriesByRegion.map((countrie) => <CountryCard data={countrie}/>)}
+      </CountryContainer>
+    </>
+  )
+}
+
+export default Home
